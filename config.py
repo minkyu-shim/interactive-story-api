afbbf4c0f26ae0e91a3f35db7a2dda24aaf0b983
Minkyu SHIM <hello.mkshim@gmail.com>
2026-02-06 13:11:39 +0100

    config to upload this project to render.com

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///nahb.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///nahb.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
